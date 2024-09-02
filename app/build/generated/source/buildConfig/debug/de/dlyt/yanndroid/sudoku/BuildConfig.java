9e0fc7eba29419f68aa8a527742fd487522ccd38
Vineelamandadi <130816516+Vineelamandadi@users.noreply.github.com>
2024-09-02 23:24:19 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/de/dlyt/yanndroid/sudoku/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.dlyt.yanndroid.sudoku;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "de.dlyt.yanndroid.sudoku";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "3.0.1";
+}
